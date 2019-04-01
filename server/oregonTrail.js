f59e61b2f92cb60e9e3abd0924015e58bdda6d1e
Mark Blankson-Hemans <mark.blankson-hemans1@marist.edu>
2019-03-31 23:04:02 -0400

    Project

--- server/oregonTrail.js
+++ server/oregonTrail.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+app.use(express.static('client/public'))
 const port = 1337
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function (req, res) { res.sendFile('index.html', {root: './client/views' })
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/mainmenu', function (req, res) { res.sendFile('mainmenu.html', {root: './client/views' })
+})
+
+app.get('/topten', function (req, res) { res.sendFile('topten.html', {root: './client/views' })
+})
+
+app.get('/setup', function (req, res) { res.sendFile('setup.html', {root: './client/views' })
+})
+
+app.get('/trail', function (req, res) { res.sendFile('trail.html', {root: './client/views' })
+})
+
+app.listen(port, () => console.log(`The app is running on port ${port}!`))
